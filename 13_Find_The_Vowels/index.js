c779f277af86520a269f4274d8bd30aa6ebe062d
AninHuang <lifeanne19904@gmail.com>
2020-02-01 15:31:25 +0800

    Add solution 2 for regular expression

--- 13_Find_The_Vowels/index.js
+++ 13_Find_The_Vowels/index.js
@@ -7,18 +7,26 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
+// Solution #2 - Regular Expression
 function vowels(str) {
-  let counter = 0;
-  //let checker = 'aeiou';
-  let checker = ['a', 'e', 'i', 'o', 'u'];
+  const matches = str.match(/[aeiou]/gi);
 
-  for (let char of str.toLowerCase()) {
-    if (checker.includes(char)) {
-      counter++;
-    }
-  }
-
-  return counter;
+  return matches ? matches.length : 0;
 }
 
 module.exports = vowels;
+
+// Solution #1 - Iteration
+// function vowels(str) {
+//   let counter = 0;
+//   //let checker = 'aeiou';
+//   let checker = ['a', 'e', 'i', 'o', 'u'];
+
+//   for (let char of str.toLowerCase()) {
+//     if (checker.includes(char)) {
+//       counter++;
+//     }
+//   }
+
+//   return counter;
+// }
