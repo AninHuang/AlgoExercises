c4406d9c9dbe1bf1ac9f4edc14d79a9e9222adaf
AninHuang <lifeanne19904@gmail.com>
2020-02-01 14:26:22 +0800

    Add solution 2

--- 11_Printing_Steps/index.js
+++ 11_Printing_Steps/index.js
@@ -18,20 +18,45 @@
 //       '### '
 //       '####'
 
-function steps(n) {
-  for (let row = 0; row < n; row++) {
-    let stair = '';
+// Solution #2 - Recursion
+function steps(n, row = 0, stair = '') {
+  //let row = 0;
+  //let stair = '';
 
-    for (let column = 0; column < n; column++) {
-      if (row >= column) {
-        stair += '#';
-      } else {
-        stair += ' ';
-      }
-    }
-
-    console.log(stair);
+  if (n === row) {
+    return;
   }
+
+  if (n === stair.length) {
+    console.log(stair);
+    steps(n, row + 1);
+    return;
+  }
+
+  if (stair.length <= row) {
+    stair += '#';
+  } else {
+    stair += ' ';
+  }
+
+  steps(n, row, stair);
 }
 
 module.exports = steps;
+
+// Solution #1 - Iteration
+// function steps(n) {
+//   for (let row = 0; row < n; row++) {
+//     let stair = '';
+
+//     for (let column = 0; column < n; column++) {
+//       if (row >= column) {
+//         stair += '#';
+//       } else {
+//         stair += ' ';
+//       }
+//     }
+
+//     console.log(stair);
+//   }
+// }
